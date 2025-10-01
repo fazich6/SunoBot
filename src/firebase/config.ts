6b5a5207a84cf1efb61227e96db98e3f02c5d561
Fazi Ch <faizannicen@gmail.com>
2025-10-01 17:15:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5224653169-6fa60",
+  "appId": "1:544234011983:web:08ed2abca3efe98765993a",
+  "apiKey": "AIzaSyA9HNQsxwH9FWBYs6mfWRAMbVd3iW2nk3Y",
+  "authDomain": "studio-5224653169-6fa60.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "544234011983"
+};
