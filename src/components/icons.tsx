3b8701a0143da48df133dc92e1db8845e73b2b69
Fazi Ch <faizannicen@gmail.com>
2025-10-04 15:50:47 +0000

    https://i.postimg.cc/ZnZZcsJ1/freepik-assistant-1757879489052-copy.png

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 export const SunoBotLogo = (props: React.SVGProps<SVGSVGElement>) => (
   <div className="flex items-center gap-2">
@@ -18,11 +19,13 @@ export const SunoBotLogo = (props: React.SVGProps<SVGSVGElement>) => (
 );
 
 export const MicIconWhite = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-        <path d="M12 2a3 3 0 0 0-3 3v7a3 3 0 0 0 6 0V5a3 3 0 0 0-3-3Z"/>
-        <path d="M19 10v2a7 7 0 0 1-14 0v-2"/>
-        <line x1="12" x2="12" y1="19" y2="22"/>
-    </svg>
+    <Image
+      src="https://i.postimg.cc/ZnZZcsJ1/freepik-assistant-1757879489052-copy.png"
+      alt="SunoBot Logo"
+      width={24}
+      height={24}
+      className={cn("invert brightness-0", props.className)}
+    />
 )
 
 export const SoundWave = (props: React.SVGProps<SVGSVGElement>) => (
